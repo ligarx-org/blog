c9f90da3bff3bb0eb5cc043fed7b05da1e089ec0
ligarx-org <ligarx6@gmail.com>
2025-10-01 04:35:02 +0200

    Updated fallback-build-manifest.json

--- out/static/chunks/webpack.js
+++ out/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "99035f06ccb1d575"; }
+/******/ 		__webpack_require__.h = function() { return "89e6da2cd1fea4f6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
